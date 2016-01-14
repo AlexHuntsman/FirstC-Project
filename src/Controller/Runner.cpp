c28967ef4970d265ba8fe49a9ed0a3ed24458017
Alexander Huntsman <Huntsman.alex101@gmail.com>
2016-01-14 09:56:37 -0700

    created first C++ project
    check modules in databases

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,24 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: ahun0442
+ */
+#include <iostream> //Using input and output streams.
+using namespace std; //Access to the Standard library
+
+int main()
+{
+	cout << "This is how you type to the console in C++" << endl;
+	//This is like System.out.println("wiehfioewhf");
+	int temp;
+
+	cin >> temp;
+
+	cout<< temp;
+
+	return 0;
+}
+
+
+
